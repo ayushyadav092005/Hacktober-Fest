77ef3fbe2f8fa24075d740ca3766d32b03483d92
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:57:26 +0530

    Update 27.java

--- Java/Easy/27.java
+++ Java/Easy/27.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i < 5; i++) {
             numbers[i] = i;
         }
     }
