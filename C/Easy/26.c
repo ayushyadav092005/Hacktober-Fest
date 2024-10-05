7be758b295d7ea41f2f3e139186dbb9227f996c7
ayushyadav092005 <143833613+ayushyadav092005@users.noreply.github.com>
2024-10-05 12:46:32 +0530

    Update 26.c

--- C/Easy/26.c
+++ C/Easy/26.c
@@ -2,8 +2,12 @@
  
 int main() {
 	int x;
+	printf("Enter the number: ");
     scanf("%d", &x); 
+	printf("Entered number is %d\n",x)
 	char c;
+	printf("Enter the character: ");
     scanf("%c", &c);
+	printf("Entered number is %s\n",c)
 	return 0;
 }
