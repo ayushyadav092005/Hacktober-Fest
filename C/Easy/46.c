b7ee97e33b7962e0b18ea8d630dda246fcf22415
ayushyadav092005 <143833613+ayushyadav092005@users.noreply.github.com>
2024-10-05 13:10:43 +0530

    Update 46.c
    Tech titans
    lang="c"

--- C/Easy/46.c
+++ C/Easy/46.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int add_numbers(int a, int b)  
-int result = a + b;  
+
 int main() {
 int num1 = 5;
 int num2 = 10;
 
-printf("%d\n", add_numbers(num1, num2));
+printf("%d %d\n", add_numbers(num1, num2));
 return 0;
 }
+int add_numbers(int a, int b)  
+ 
 
