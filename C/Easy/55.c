79d8b45de6ed14bfcebb91e52009c3d33d396033
ayushyadav092005 <143833613+ayushyadav092005@users.noreply.github.com>
2024-10-05 13:32:11 +0530

    Update 55.c

--- C/Easy/55.c
+++ C/Easy/55.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
-    printf("%d\n", arr[5]);
+    for(i=0;i<5;i++){
+        arr[5]=arr[i];
+        printf("%d\n", arr[5]);
+    }
     return 0;
 }
 
