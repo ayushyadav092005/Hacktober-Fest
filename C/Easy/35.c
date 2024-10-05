e9079b32f4580fd2907918109ce22063c5ce06f5
ayushyadav092005 <143833613+ayushyadav092005@users.noreply.github.com>
2024-10-05 13:12:05 +0530

    Update 35.c

--- C/Easy/35.c
+++ C/Easy/35.c
@@ -2,22 +2,22 @@
 
 int main() {
     
-    int array1[] = {1, 2, 3}; 
-    int array2[] = {4, 5, 6}; 
+    int array1[3] = {1, 2, 3}; 
+    int array2[3] = {4, 5, 6}; 
     int concatenated[6];     
 
     
-    for (int i = 0; i < __; i++) {
-       _______ = array1[i];
+    for (int i = 0; i < 3; i++) {
+       concatenated[i] = array1[i];
     }
 
    
     for (int i = 0; i < 3; i++) {
-        concatenated[____] = array2[i];
+        concatenated[i] = array2[i];
     }
 
  
-    for (int i = 0; i < __; i++) {
+    for (int i = 0; i < 3; i++) {
         printf("%d ", concatenated[i]);
     }
     printf("\n");
