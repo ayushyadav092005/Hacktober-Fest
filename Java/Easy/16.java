bab436405f1cd2760f18262193e66c16fc9ea6be
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:44:01 +0530

    Update 16.java

--- Java/Easy/16.java
+++ Java/Easy/16.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a > b ? "Greater" : "Smaller");
+        if System.out.println(a > b ? "Greater" : "Smaller");
         else System.out.println("Equal");
     }
 }
