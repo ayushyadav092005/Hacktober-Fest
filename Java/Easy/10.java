d5793682874e96cb56e340a28f2be747b2192d14
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:30:54 +0530

    Update 10.java

--- Java/Easy/10.java
+++ Java/Easy/10.java
@@ -1,7 +1,9 @@
-class Main {
-    public static void main(String[] args) {
+class Main
+{
+    public static void main(String[] args)
+    {
         String name = "John";
-        char letter = name.charAt(5);
+        char letter = name.charAt(3);
         System.out.println(letter);
     }
 }
