ac958a80311af5edcbcc1f20331afe4858fc3d36
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 13:39:42 +0530

    Update 60.java

--- Java/Easy/60.java
+++ Java/Easy/60.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        int a = 10, b = 0;
+        int a = 10, b = 1;
         int result = a / b;
         System.out.println(result);
     }
