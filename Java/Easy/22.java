6fe30999bb7fc0acbb15b0a333efead5ff5c8867
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 12:53:30 +0530

    Update 22.java

--- Java/Easy/22.java
+++ Java/Easy/22.java
@@ -1,6 +1,7 @@
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[] arr = new int[5];
-        System.out.println(arr.size());
+        System.out.println(arr.size);
     }
 }
