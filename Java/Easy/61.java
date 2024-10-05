cc1194103de43751e4dd522b7530c5e9678dcd48
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 13:42:05 +0530

    Update 61.java

--- Java/Easy/61.java
+++ Java/Easy/61.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        for (int i : arr) {
+        for (int i=1 ; i<arr.size;i++) {
             arr[i] = i * 2;
         }
     }
