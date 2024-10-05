249157a131133e11ecd1e8c152dd78e6f4060188
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 13:10:41 +0530

    Update 38.java

--- Java/Easy/38.java
+++ Java/Easy/38.java
@@ -1,6 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        System.out.println(arr(0));
+        arr[0]=0;
+        System.out.println(arr[0]);
     }
 }
