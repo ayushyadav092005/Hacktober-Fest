a0c6514bb72f13f6baf135c2ae16ae27db44f62f
ayushyadav092005 <ayushyadav092005@gmail.com>
2024-10-05 13:22:44 +0530

    Update 47.java

--- Java/Easy/47.java
+++ Java/Easy/47.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int a = 5;
-        int result = Math.pow(a);
+        int result = Math.pow(a,a);
         System.out.println(result);
     }
 }
